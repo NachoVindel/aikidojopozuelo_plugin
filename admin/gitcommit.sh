1d4015c845ec6e17374a514aeb0d4a91085a9134
Nacho Vindel <nacho.vindel@gmail.com>
2017-12-14 15:06:35 +0000

    20171214-150635

--- admin/gitcommit.sh
+++ admin/gitcommit.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-if (( $# < 1 )); then
-    git add -A 
-    git commit -m $(date +%Y%m%d-%H%M%S)
-    git push origin master
-else
-
-    git add -A
-    git commit -m $1
-    git push origin master
-
-fi
+git add -A 
+git commit -m $(date +%Y%m%d-%H%M%S)
+git push origin master
