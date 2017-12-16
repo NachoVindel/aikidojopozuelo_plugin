e11a24a88b4bd2899d154f4d3613d5096359d0f7
Nacho Vindel <nacho.vindel@gmail.com>
2017-12-16 01:22:26 +0000

    20171216-012226

--- includes/adp/adp_funciones.php
+++ includes/adp/adp_funciones.php
@@ -83,7 +83,7 @@ class adp_Alumno
     
     private function __construct1($idAlumno) 
     {
-        echo 'in ---  -> '.$idAlumno;
+        echo 'in-'.$idAlumno;
         
         $wpUser = adp_Alumno::UsuarioWP($id_alumno);
         
