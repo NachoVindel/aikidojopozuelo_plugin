5d37c4a6a1ff81f486a2d465354aad54025d64ba
Nacho Vindel <nacho.vindel@gmail.com>
2018-07-10 15:04:10 +0000

    20180710-150410

--- includes/adp/adp_funciones.php
+++ includes/adp/adp_funciones.php
@@ -312,13 +312,13 @@ function adp_lista($usuarios, $message)
     {
         $out.='<ul style="list-style-type: none">';
     	foreach ( $usuarios as $usuario )
-    	{
-    		$out.= '<li><a '; 
+    	{	
+    		$out.= '<li><a href="/admin-dojo/?accion=ficha_alumno&id_alumno=' . $usuario->id . '">';
     		
     		if (!$usuario->licencia_aikikan)
-    		    $out.= ' style="text-decoration:color:red" ';
+    		    $out.= esc_html('(*)');
     		
-    		$out.= ' href="/admin-dojo/?accion=ficha_alumno&id_alumno=' . $usuario->id . '">' . esc_html( $usuario->first_name ) . ' ' . esc_html( $usuario->last_name ) 
+    		$out.= esc_html( $usuario->first_name ) . ' ' . esc_html( $usuario->last_name ) 
     		. '</a> (<a href="/admin-dojo/?accion=activar_alumno&id_alumno=' . $usuario->id . '">Activar</a> ' 
     		. '- <a href="/admin-dojo/?accion=desactivar_alumno&id_alumno=' . $usuario->id . '">Desactivar</a> '
     		. '- <a href="/wp-admin/user-edit.php?user_id=' . $usuario->id . '&wp_http_referer=%2Fadmin%2">Editar</a>)';
