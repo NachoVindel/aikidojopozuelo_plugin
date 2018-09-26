f344e20ca78bb5b7e2185c8819050e1e8cf7dcfd
Nacho Vindel <nacho.vindel@gmail.com>
2018-09-26 19:35:45 +0000

    20180926-193545

--- includes/adp/adp_funciones.php
+++ includes/adp/adp_funciones.php
@@ -114,6 +114,7 @@ class adp_Alumno
         $this->FechaNidan = $this->FechaAlumno($wpUser->fecha_nidan);
         $this->FechaSandan = $this->FechaAlumno($wpUser->fecha_sandan);
         $this->FechaYondan = $this->FechaAlumno($wpUser->fecha_yondan);
+        
     }
         
     
@@ -136,6 +137,12 @@ class adp_Alumno
     {
         $user = get_userdata($idAlumno);
 	    $user->set_role( 'alumno' );
+    }
+    
+    public static function CrearBlog($idAlumno)
+    {
+        $user = get_userdata($idAlumno);
+        adp_crear_blog_de_alumno($user);
     }
     
     public static function ListaAlumnos()
@@ -434,6 +441,79 @@ function code_user_datediff($atts)
 }
 */
 
+function adp_crear_blog_de_alumno($alumno)
+{
+    $leadTitle = 'Blog Personal de ' . $alumno->NombreCompleto . '(' . $alumno->DNI . ')';
+    
+    if (get_page_by_tittle($leadTitle) != null)
+        return;
+
+	/*******************************************************
+	** POST VARIABLES
+	*******************************************************/
+
+	$postType = 'post'; // set to post or page
+	$userID = 1; // set to user id
+	$categoryID = '24'; // set to category id.
+	$postStatus = 'publish';  // set to future, draft, or publish
+
+	$leadContent = '<h1>Blog Personal</h1><p>En este blog intercambiaremos la información sobre tu evolución</p>';
+
+	/*******************************************************
+	** TIME VARIABLES / CALCULATIONS
+	*******************************************************/
+	// VARIABLES
+	$timeStamp = $minuteCounter = 0;  // set all timers to 0;
+	$iCounter = 1; // number use to multiply by minute increment;
+	$minuteIncrement = 1; // increment which to increase each post time for future schedule
+	$adjustClockMinutes = 0; // add 1 hour or 60 minutes - daylight savings
+
+	// CALCULATIONS
+	$minuteCounter = $iCounter * $minuteIncrement; // setting how far out in time to post if future.
+	$minuteCounter = $minuteCounter + $adjustClockMinutes; // adjusting for server timezone
+
+	$timeStamp = date('Y-m-d H:i:s', strtotime("+$minuteCounter min")); // format needed for WordPress
+
+	/*******************************************************
+	** WordPress Array and Variables for posting
+	*******************************************************/
+
+	$new_post = array(
+		'post_title' => $leadTitle,
+		'post_content' => $leadContent,
+		'post_status' => $postStatus,
+		'post_date' => $timeStamp,
+		'post_author' => $userID,
+		'post_type' => $postType,
+		'post_category' => array($categoryID)
+		);
+
+	/*******************************************************
+	** WordPress Post Function
+	*******************************************************/
+
+	$post_id = wp_insert_post($new_post);
+
+	/*******************************************************
+	** SIMPLE ERROR CHECKING
+	*******************************************************/
+/*
+	$finaltext = '';
+
+	if($post_id){
+	
+		$finaltext .= 'Yay, I made a new post.<br>';
+
+	} else{
+
+		$finaltext .= 'Something went wrong and I didn\'t insert a new post.<br>';
+
+	}
+
+	echo $finaltext;
+*/
+}
+
 function code_adp_ficha_alumno($idAlumno)
 {
     $iAdmin = false;
@@ -527,6 +607,7 @@ function code_adp_acciones_admin()
             if ($id_alumno != '')
             {
                 adp_Alumno::Activar($id_alumno);
+                adp_Alumno::CrearBlog($id_alumno);
             }
             $out = adp_ListaAlumnos();
             break;
