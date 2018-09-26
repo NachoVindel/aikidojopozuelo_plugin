9ae0f19b2482166d21215685e586cef44cfed5c5
Nacho Vindel <nacho.vindel@gmail.com>
2018-09-26 23:31:11 +0000

    20180926-233111

--- includes/adp/adp_funciones.php
+++ includes/adp/adp_funciones.php
@@ -502,7 +502,7 @@ function adp_crear_blog_de_alumno($alumno)
     $post_id = -1;
 	$post_id = wp_insert_post($new_post);
 	if ($post_id != -1) 
-	    update_user_meta( $alumno->ID, "id_personal_blog", $post_id);
+	    update_user_meta( $alumno->ID, 'id_personal_blog', $post_id);
 
 	/*******************************************************
 	** SIMPLE ERROR CHECKING
